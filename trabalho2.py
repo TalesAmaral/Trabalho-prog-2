6019c2823d74ab3bd7ac6be68122590dd382000a
Tales <talessilvaamaral@gmail.com>
2022-12-09 18:53:55 -0300

    adicionando questao 2

--- trabalho2.py
+++ trabalho2.py
@@ -10,8 +10,18 @@ def produto(mA,mB):
                 soma+= mA[i][k]*mB[k][g]
             mR[i].append(soma)
     return mR
-print(produto([[2,3],[1,0],[4,5]],[[3,1],[2,4]]))
 
+def isEqual(mA,mB):
+    if len(mA)!=len(mB):
+        return 0
+    if len(mA[0]) != len(mB[0]):
+        return 0
+    for i in range(len(mA)):
+        for g in range(len(mB)):
+            if mA[i][g] != mB[i][g]:
+                return 0
+    return 1
+print(isEqual([[1,2],[3,4]],[[1,2],[1,4]]))
             
 
 
